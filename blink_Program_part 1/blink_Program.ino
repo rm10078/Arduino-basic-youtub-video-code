ccc40b7c66e025db80c4cde43809195a1ac4b119
rajib <rm10078@gmail.com>
2021-02-27 16:39:24 +0530

    first commit

---
+++ blink_Program_part 1/blink_Program.ino
@@ -0,0 +1,15 @@
+const int led=13;
+float w=500.34;
+long b=45145145.5657;
+boolean y=true;
+void setup(){
+  //this is a setup function
+  pinMode(led,OUTPUT);
+  }
+void loop(){
+  int t=500;
+  digitalWrite(led,HIGH);
+  delay(t);
+  digitalWrite(led,LOW);
+  delay(t);
+  }
